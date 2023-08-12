06605134a0549fd8600c5cb8cb70004dcc84672a
deeptinuthakki <142028713+deeptinuthakki@users.noreply.github.com>
2023-08-12 15:54:55 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-
 
 #take the json and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
