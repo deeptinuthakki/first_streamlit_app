fb253994659439f51d8b7f40718f91e3203f0b83
deeptinuthakki <142028713+deeptinuthakki@users.noreply.github.com>
2023-08-16 14:04:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if not fruit_choice:
 except URLError as e:
 streamlit.error()
 
-streamlit.write('The user entered ', fruit_choice)
+
 
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
