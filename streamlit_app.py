58e6d8f1fc398041626f69315110e9258548cca3
deeptinuthakki <142028713+deeptinuthakki@users.noreply.github.com>
2023-08-16 15:56:46 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,24 +23,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
- #create the repeatable code block (called a function)
- def get_fruityvice_data (this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
-
-#NEW SECTION TO DISPLAY FRUITYVICE API RESPONSE
+#New section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text.input('What fruit would you like information about?')
 if not fruit_choice:
-  streamlit.error("Please select a fruit to get information.")
+ streamlit.error("Please select a fruit to get information")
 else:
-  back_from_function = get_fruityvice_data(fruit_choice)
-  streamlit.dataframe(back_from_function)
+ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+ fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
+ streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-streamlit.error()
+ streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
@@ -57,7 +52,8 @@ streamlit.dataframe(my_data_rows)
 #adding new table
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding', fruit_choice)
+streamlit.write('Thanks for adding', add_my_fruit)
+
 
 
 
